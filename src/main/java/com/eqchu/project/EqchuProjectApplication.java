a36934cd9896f83c475b9d33d0cf8463188e64ad
DancyLon <alexdancylon@gmail.com>
2020-04-02 22:57:12 +0800

    first commit

---
+++ src/main/java/com/eqchu/project/EqchuProjectApplication.java
@@ -0,0 +1,13 @@
+package com.eqchu.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqchuProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EqchuProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eqchu/project/EqchuProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eqchu.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EqchuProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
